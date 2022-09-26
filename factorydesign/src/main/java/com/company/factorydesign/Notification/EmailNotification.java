da466a28758ff020795101a2f9cb5459e9a7aa5d
Anand_Mishra <mishraanand158@gmail.com>
2022-09-26 21:03:25 +0530

    "Factory Design Pattern"

---
+++ factorydesign/src/main/java/com/company/factorydesign/Notification/EmailNotification.java
@@ -0,0 +1,8 @@
+package com.company.factorydesign.Notification;
+
+public class EmailNotification implements Notification{
+    @Override
+    public void notifyUser() {
+        System.out.println(" Email Notification executed ");
+    }
+}
